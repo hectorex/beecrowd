3f1a4ff0622b814c355fd34c1ad9e3c0014d84c0
hectorex <celsohectorhm@gmail.com>
2024-10-01 19:09:49 -0400

    terminar

---
+++ somadeimparesconsecutivos2.py
@@ -0,0 +1,15 @@
+x = int(input())
+
+for c in range(x):
+    soma = 0
+    splitas = input().split(" ")
+    x,y = map(int, splitas)
+    if y > x:
+        for c in range (x+1,y):
+            if c%2 != 0:
+                soma += c  
+    else:
+        for c in range (y+1,x):
+            if c%2 != 0:
+                soma =  11
+    print(soma)
