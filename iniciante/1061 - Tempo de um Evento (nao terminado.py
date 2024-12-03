bf65b1d885789957565770f00fd76d0d551eeb9b
hectorex <celsohectorhm@gmail.com>
2024-12-03 11:46:40 -0300

    tem q enfrentar os medos: questao 1061

--- iniciante/1061 - Tempo de um Evento (nao terminado.py
+++ iniciante/1061 - Tempo de um Evento (nao terminado.py
@@ -1,27 +1,28 @@
-firstday = str(input()).split(" ")
-firstday_dados = str(input()).split(" : ")
-finalday = str(input()).split(" ")
-finalday_dados = str(input()).split(" : ")
+#fazeeeeeeeeeeeer
+# firstday = str(input()).split(" ")
+# firstday_dados = str(input()).split(" : ")
+# finalday = str(input()).split(" ")
+# finalday_dados = str(input()).split(" : ")
 
-dia, numero_dia = map(str, firstday)
-numero_dia = int(numero_dia)
+# dia, numero_dia = map(str, firstday)
+# numero_dia = int(numero_dia)
 
-horas, minutos, segundos = map(str, firstday)
-horas = int(horas)
-minutos = int(minutos)
-segundos = int(segundos)
+# horas, minutos, segundos = map(str, firstday)
+# horas = int(horas)
+# minutos = int(minutos)
+# segundos = int(segundos)
 
-dia, numero_dia2 = map(str, firstday)
-numero_dia2 = int(numero_dia2)
+# dia, numero_dia2 = map(str, firstday)
+# numero_dia2 = int(numero_dia2)
 
-horas2, minutos2, segundos2 = map(str, firstday)
-horas2 = int(horas2)
-minutos2 = int(minutos2)
-segundos2 = int(segundos2)
+# horas2, minutos2, segundos2 = map(str, firstday)
+# horas2 = int(horas2)
+# minutos2 = int(minutos2)
+# segundos2 = int(segundos2)
 
-dias = numero_dia - numero_dia2
-horasreal = abs(24 - (horas - horas2))
-minutosreal = abs(minutos - minutos2)
-segundosreal = abs(segundos - segundos2)
+# dias = numero_dia - numero_dia2
+# horasreal = abs(24 - (horas - horas2))
+# minutosreal = abs(minutos - minutos2)
+# segundosreal = abs(segundos - segundos2)
 
-print(dias, horasreal, minutosreal, segundosreal)
+# print(dias, horasreal, minutosreal, segundosreal)
