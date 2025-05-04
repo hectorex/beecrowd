702422c05f3a183f912aefdb58802093b408e1f9
Celso Hector <132790439+hectorex@users.noreply.github.com>
2025-05-04 12:20:36 -0400

    Atualizar o 3049 - Nota Cortada.py
    dentro no carro no celular

--- matematica/3049 - Nota Cortada.py
+++ matematica/3049 - Nota Cortada.py
@@ -1,1 +1,11 @@
-#fazer
+a = int(input())
+b = int(input())
+retangulo = 160*70
+trapezio = ((a+b)*70)/2
+if trapezio == retangulo/2:
+    rslt = 0
+elif trapezio > retangulo/2:
+    rslt = 1
+else:
+    rslt = 2
+print(rslt)
