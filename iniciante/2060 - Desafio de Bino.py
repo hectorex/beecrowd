0c5c8890b0a3b407a9773d621ea1c3a675e1def0
hectorex <celsohectorhm@gmail.com>
2025-07-14 20:37:31 -0300

    nova descoberta: EOFerror

--- iniciante/2060 - Desafio de Bino.py
+++ iniciante/2060 - Desafio de Bino.py
@@ -1,2 +1,19 @@
 descart = int(input())
 valores = [int(x) for x in input().split()]
+
+mult_2 = 0
+mult_3 = 0
+mult_4 = 0
+mult_5 = 0
+
+for c in range(len(valores)):
+    if valores[c]%2 == 0:
+        mult_2 += 1
+    if valores[c]%3 == 0:
+        mult_3 += 1
+    if valores[c]%4 == 0:
+        mult_4 += 1
+    if valores[c]%5 == 0:
+        mult_5 += 1
+
+print(f"{mult_2} Multiplo(s) de 2\n{mult_3} Multiplo(s) de 3\n{mult_4} Multiplo(s) de 4\n{mult_5} Multiplo(s) de 5")
