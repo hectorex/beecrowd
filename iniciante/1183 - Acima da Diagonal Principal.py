8c6acfb0158b34a28122eb080d1a4c6a1eec8ac4
hectorex <celsohectorhm@gmail.com>
2026-05-27 16:38:15 -0400

    bee 1183 refactoring

--- iniciante/1183 - Acima da Diagonal Principal.py
+++ iniciante/1183 - Acima da Diagonal Principal.py
@@ -1,5 +1,4 @@
 #1183 - Acima da Diagonal Principal
-
 char = input()
 
 matriz = []
@@ -7,6 +6,18 @@ total = 0.0
 for i in range(12):
     matriz.append([float(input()) for _ in range(12)])
 
+for i, line in enumerate(matriz):
+    for j in range(12):
+        if j > i:
+            total += line[j]
+
+if char == 'M':
+    total /= 66
+
+print(f"{total:.1f}")
+
+'''
+GAMBIARRA Q ESTAVA NO LUGAR DO ENUMERATE
 index = 0
 for line in matriz:
     for y in range(12):
@@ -14,7 +25,4 @@ for line in matriz:
             total += line[y]
     index += 1
 
-if char == 'M':
-    total /= 66
-
-print(f"{total:.1f}")
+'''
