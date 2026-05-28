70fd2486a5b3092e1156c3af13ce0457f637f046
hectorex <celsohectorhm@gmail.com>
2026-05-27 20:53:31 -0400

    bee 1186

---
+++ iniciante/1186 - Abaixo da Diagonal Secundária.py
@@ -0,0 +1,28 @@
+#1186 - Acima da Diagonal Secundária
+
+matriz = []
+char = input()
+total = 0.0
+
+for c in range(12):
+    matriz.append([float(input()) for _ in range(12)])
+
+for j, line in enumerate(matriz):
+    for i, valor in enumerate(line):
+        if i + j > 11:
+            total += valor
+
+if char == 'M':
+    total /= 66.0
+
+print(f"{total:.1f}")
+
+'''
+index = 11
+for j, line in enumerate(matriz):
+    for i in range(12):
+        if i < index:
+            total += line[i]
+    index -= 1
+
+'''
