dfba39486076325ffd531c5d2546a103316ef0d5
hectorex <celsohectorhm@gmail.com>
2026-05-27 21:33:31 -0400

    bee 1187

---
+++ iniciante/1187 - Area Superior.py
@@ -0,0 +1,21 @@
+# 1187 - Área Superior
+
+matriz = []
+
+ope = input()
+total = 0.0
+
+for i in range(12):
+    matriz.append([float(input()) for _ in range(12)])
+
+cont = 0
+for i, line in enumerate(matriz):
+    for j, number in enumerate(line):
+        if j > 0+cont and j < 11-cont:
+            total += line[j]
+    cont +=1
+
+if ope == 'M':
+    total /= 30
+
+print(f'{total:.1f}')
