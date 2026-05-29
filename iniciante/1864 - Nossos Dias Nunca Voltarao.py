7017db92b48b3d15ec2a39bfa2115f9ecec87f65
hectorex <celsohectorhm@gmail.com>
2026-05-28 20:31:59 -0400

    bee 1864

---
+++ iniciante/1864 - Nossos Dias Nunca Voltarao.py
@@ -0,0 +1,4 @@
+vet = ['L','I','F','E',' ','I','S',' ','N','O','T',' ','A',' ','P','R','O','B','L','E','M',' ','T','O',' ','B','E',' ','S','O','L','V','E','D']
+for c in range(int(input())):
+    print(vet[c], end='')
+print()
