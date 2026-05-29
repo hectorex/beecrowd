7476fe03dd8c7c6a8f811a963206ea2d8d4d16d2
hectorex <celsohectorhm@gmail.com>
2026-05-29 12:37:26 -0400

    bee 2235 in C

---
+++ iniciante/2235-AndandoNoTempo.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int v[3];
+    scanf("%d %d %d", &v[0], &v[1], &v[2]);
+    if (v[0] - v[1] == 0 || v[0] - v[2] == 0 || v[1]-v[2] == 0) {
+        printf("S\n");
+    }
+
+    else if(v[0] + v[1] == v[2] || v[0] + v[2] == v[1] || v[1]+v[2] == v[0]){
+        printf("S\n");
+    }
+
+    else {
+        printf("N\n");
+    }
+}
