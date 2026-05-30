5d3fd562c4c73b0ad901149c152d662021fb4d9f
hectorex <celsohectorhm@gmail.com>
2026-05-30 11:12:39 -0400

    bee 2896 in c

---
+++ iniciante/2896-AproveiteAOferta.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+    int t, v[2], total;
+    scanf("%d", &t);
+    for (int i = 1; i<=t; i++) {
+        scanf("%d %d", &v[0], &v[1]);
+        total = v[0]-(v[0]/v[1])*v[1]+(v[0]/v[1]);
+        printf("%d\n", total);
+    }
+
+    return 0;
+}
