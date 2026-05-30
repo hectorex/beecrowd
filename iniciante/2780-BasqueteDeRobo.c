59368e460dc7f84f7830bccfb220807054b061b0
hectorex <celsohectorhm@gmail.com>
2026-05-29 21:33:03 -0400

    bee 2780 in c

---
+++ iniciante/2780-BasqueteDeRobo.c
@@ -0,0 +1,18 @@
+#include <stdio.h>    
+
+int main() {
+    int num, pnts;
+    scanf("%d", &num);
+
+    if (num <= 800){
+        pnts = 1;
+    }
+    else if (num <= 1400){
+        pnts = 2;
+    }
+    else if (num <= 2000){
+        pnts = 3;
+    }
+    printf("%d", pnts);
+    return 0;
+}
