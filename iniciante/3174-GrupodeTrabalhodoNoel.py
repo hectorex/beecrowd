5a78fd10d814ee7b59bb8c2340ec75119e2a8a44
hectorex <celsohectorhm@gmail.com>
2026-06-02 12:55:16 -0400

    correct commit 3174

--- iniciante/3174-GrupodeTrabalhodoNoel.py
+++ iniciante/3174-GrupodeTrabalhodoNoel.py
@@ -8,13 +8,13 @@ p_d = 0
 for c in range(int(input())):
     e, g, h = (str(x) for x in str(input()).split())
     h = int(h)
-    if (g=="bonecos" and h >= 8):
+    if (g=="bonecos"):
         p_b += h
-    elif (g=="arquitetos" and h >= 4):
+    elif (g=="arquitetos"):
         p_a += h
-    elif (g=="musicos" and h >= 6):
+    elif (g=="musicos"):
         p_m += h
-    elif (g=="desenhistas" and h >= 12):
+    elif (g=="desenhistas"):
         p_d += h
 
 p = (p_b//8)+(p_a//4)+(p_m//6)+(p_d//12)
